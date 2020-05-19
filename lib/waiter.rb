d60c4af435b889cc15cefe088d2fa2bb35643c40
claudia v <claudia4ro@yahoo.com>
2020-05-19 20:59:29 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -7,10 +7,11 @@ class Waiter
   def initialize(name, yrs_experience)
     @name = name 
     @yrs_experience = yrs_experience
-    @@all << self
+    
   end 
   
   def self.all 
+    @@all << self
     @all
   end 
   
